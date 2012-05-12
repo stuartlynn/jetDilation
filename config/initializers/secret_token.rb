63bfd459456488b8c9c0537574404a7e42b719f2
Stuart Lynn <stuart.lynn@gmail.com>
2012-05-12 15:05:28 -0500

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JetDilation::Application.config.secret_token = '2c337f08eee87084f2be3b59e5e47aae10ead80bb2594552c283c61bbacc6049c658824b52c52b1cd8641c53ba23023bcccb3e8e5a522520da0f527831f52731'
